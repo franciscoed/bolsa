87d6ebe059b80182488f8ba9a3c67fae16a9b603
Francisco Silva <franciscoed@gmail.com>
2016-08-14 20:52:28 -0300

    Meetup Version

--- lambda-csv.py
+++ lambda-csv.py
@@ -10,38 +10,32 @@ import urllib
 print('Loading function')
 
 s3 = boto3.client('s3')
+lista = ('RENT3', 'KLBN4', 'ABEV3', 'BVMF3', 'EMBR3', 'HYPE3')
 
 
 
 def downloadCSV (stock):
-    url = 'http://www.google.com/finance/historical?q=BVMF%3ARENT3&output=csv'
-    #url = 'https://www.google.com/finance/historical?q=rent3&output=csv'
-    #url = 'http://www.google.com/finance/historical?q=' + urllib.quote('rent3') + '&histperiod=' + urllib.quote('daily') + \
-    #'&startdate=' + urllib.quote('Aug+8+2015') + '&enddate=' + urllib.quote('Aug+8+2016') + '&output=' + urllib.quote('csv')
+    url = 'http://www.google.com/finance/historical?q=BVMF%3A' + stock + '&output=csv'
 
     print(url)
     try:
-
         response = urllib2.urlopen(url).read()
-        print(response)
     except Exception as e:
-        print(e)
         raise e
     return response
 
 
 def lambda_handler(event, context):
-    bucket = 'chicolambdatest'
-    key = 'rent3'
-    csv=downloadCSV(key)
+    bucket = 'meetup1608'
 
-    try:
-        response = s3.put_object(Bucket=bucket, Key=key, Body=csv)
-        print('upload ok')
-        print(csv)
-        #return response
+    for i in range(len(lista)):
+        key=lista[i]
+        csv=downloadCSV(key)
+        try:
+            response = s3.put_object(Bucket=bucket, Key=key, Body=csv)
+            print('upload {} ok'.format(key))
 
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
+        except Exception as e:
+            print('Error putting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+            raise e
+    return 0
